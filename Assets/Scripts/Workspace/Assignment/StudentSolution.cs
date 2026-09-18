9a3533062103836142deeab2eca07ebcd1961f4c
Lasksn <laksn.than@bumail.net>
2026-09-18 17:05:42 +0700

    Assiment

--- Assets/Scripts/Workspace/Assignment/StudentSolution.cs
+++ Assets/Scripts/Workspace/Assignment/StudentSolution.cs
@@ -3,6 +3,8 @@ using System.Reflection;
 using System.Collections.Generic;
 using System.Collections;
 using System.Linq;
+using System.Runtime.ExceptionServices;
+using System;
 
 namespace Assignment
 {
@@ -11,16 +13,66 @@ namespace Assignment
         #region Lecture
         public int[] LCT01_SelectionSortAscending(int[] numbers)
         {
+            int n = numbers.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (numbers[j] < numbers[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+               
+                //int temp = numbers[minIndex];
+                //numbers[minIndex] = numbers[i];
+                //numbers[i] = temp;
+                (numbers[minIndex], numbers[i]) = (numbers[i], numbers[minIndex]);
+            }
+            foreach (var n_ in numbers)
+            {
+                Debug.Log(n_);
+            }
             return numbers;
         }
 
         public int[] LCT02_BubbleSortAscending(int[] numbers)
         {
-            return numbers;
+            int n = numbers.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+            }
+                return numbers;
         }
 
         public int[] LCT03_InsertionSortAscending(int[] numbers)
         {
+            int n = numbers.Length;
+            for (int i = 1; i < n; i++)
+            {
+                int key = numbers[i];
+                int j = i - 1;
+                while (j >= 0 && numbers[j] > key)
+                {
+                    numbers[j + 1] = numbers[j];
+                    j--;
+                }
+                numbers[j + 1] = key;
+            }
+            foreach (var n_ in numbers)
+            {
+                Debug.Log(n_);
+            }
             return numbers;
         }
 
@@ -30,22 +82,81 @@ namespace Assignment
 
         public int[] AS01_SelectionSortDescending(int[] numbers)
         {
+            int n = numbers.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int maxIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    
+                    if (numbers[j] > numbers[maxIndex])
+                    {
+                        maxIndex = j;
+                    }
+                }
+               
+                (numbers[maxIndex], numbers[i]) = (numbers[i], numbers[maxIndex]);
+            }
             return numbers;
         }
 
         public int[] AS02_BubbleSortDescending(int[] numbers)
         {
+            int n = numbers.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (numbers[j] < numbers[j + 1])
+                    {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+            }
             return numbers;
         }
 
         public int[] AS03_InsertionSortDescending(int[] numbers)
         {
+            int n = numbers.Length;
+            for (int i = 1; i < n; i++)
+            {
+                int key = numbers[i];
+                int j = i - 1;
+                while (j >= 0 && numbers[j] < key)
+                {
+                    numbers[j + 1] = numbers[j];
+                    j--;
+                }
+                numbers[j + 1] = key;
+            }
             return numbers;
         }
 
         public int AS04_FindTheSecondLargestNumber(int[] numbers)
         {
-            return 0;
+            int n = numbers.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int maxIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (numbers[j] == numbers[maxIndex])
+                    {
+                        numbers[j] = 0;
+                    }
+                    if (numbers[j] > numbers[maxIndex])
+                    {
+                        maxIndex = j;
+                    }
+                }
+
+                (numbers[maxIndex], numbers[i]) = (numbers[i], numbers[maxIndex]);
+            }
+
+            return numbers[1];
         }
 
         #endregion
--- Assets/Workshop/Student/Scripts/OOPPlayer.cs
+++ Assets/Workshop/Student/Scripts/OOPPlayer.cs
@@ -46,6 +46,17 @@ namespace Solution
             if (inventory.HasItem("FireStorm",1))
             {
                 //stundent exercise: use FireStorm to attack 3 lower energy enemies on map
+                inventory.UseItem("FireStorm", 1);
+                OOPEnemy[] enemies = SortEnemiesByRemainningEnergy2();
+                int count = 3;
+                if (count > enemies.Length)
+                {
+                    count = enemies.Length;
+                }
+                for (int i = 0; i < count; i++)
+                {
+                    enemies[i].TakeDamage(10);
+                }
             }
             else
             {
@@ -56,7 +67,28 @@ namespace Solution
         {
             var enemies = mapGenerator.GetEnemies();
             //stundent exercise: sort enemies by remainning energy
+            //for (int i = 0; i < enemies.Length - 1; i++) {
+                //int minIndex = i;
+            
+                //for (int j = i +1; j < enemies.Length; j++)
+               // {
+                 //   if (enemies[j].energy < enemies[minIndex].energy)
+                 //   {
+                 //       minIndex = j;
+                //    }
+                 //   (enemies[i], enemies[minIndex]) = (enemies[minIndex], enemies[i]);
 
+               // }
+
+           // }
+           Array.Sort(enemies, (a, b) => a.energy.CompareTo(b.energy));
+           //Array.Sort(enemies, (a, b) =>
+          // {
+           //    if (a.energy < b.energy) { return -1; }
+           //    else if (a.energy > b.energy) { return 1; }
+           //    else { return 0; }
+
+          // });
             return enemies;
         }
 
